86a09ea6bf7daa29dce83aa05a28f14e7e981a74
lukasz janik <jlukasz72@gmail.com>
2022-04-19 22:50:41 +0200

    Components

---
+++ src/components/Home/index.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['Ł', 'u', 'k', 'a', 's', 'z',' ', 'J', 'a', 'n', 'i', 'k']
+  const jobArray = [
+    'F',
+    'u',
+    'l',
+    'l',
+    ' ',
+    'S',
+    't',
+    'a',
+    'c',
+    'k',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={20}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={10}
+            />
+          </h1>
+          <h2>Full Stack Developer | Tukano Software House </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
